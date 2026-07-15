c97e00c029b70ef26e60a386a1dbb6311d286bcd
chinmaysubandh <chinmaysubandh@gmail.com>
2026-07-15 15:06:36 +0530

    Register user endpoint complete

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import { json } from "node:stream/consumers";
-
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use("/api/auth", authRoutes);
 
 app.get("/", (req, res) => {
     res.json({
---
+++ server/src/controllers/auth.controller.ts
@@ -0,0 +1,55 @@
+import { Request, Response } from "express";
+import bcrypt from "bcrypt";
+import User from "../models/User";
+
+export const registerUser = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+      return;
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      res.status(409).json({
+        success: false,
+        message: "User already exists",
+      });
+      return;
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      message: "Internal Server Error",
+    });
+  }
+};
